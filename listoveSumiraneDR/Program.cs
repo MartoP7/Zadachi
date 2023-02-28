ab8726d3f8e1c4b58e053a0ee6441dfd56f0bfe0
MartoP7 <martin.papazov489@edu1.pgzaimov.com>
2023-02-28 08:29:26 +0200

    zad1

---
+++ listoveSumiraneDR/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace listoveSumiraneDR
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // INPUT
+
+            int n = int.Parse(Console.ReadLine());
+            List<int> list = new List<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                list.Add(int.Parse(Console.ReadLine()));
+            }
+
+            // OUTPUT
+
+            Console.WriteLine();
+            int sum = 0;
+            for (int i = 0; i < list.Count; i++)
+            {
+                int Reverse = 0;
+                while (list[i] > 0)
+                {
+                    int remainder = list[i] % 10;
+                    Reverse = (Reverse * 10) + remainder;
+                    list[i] = list[i] / 10;
+                }
+                sum = sum + Reverse;
+            }
+
+            Console.WriteLine($"Sum: {sum}");
+
+        }
+    }
+}
