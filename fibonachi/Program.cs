56f625abb0d26507bb97c8d22cd2bb1de17a9b67
MartoP7 <martin.papazov489@edu1.pgzaimov.com>
2023-03-01 11:16:41 +0200

    update

---
+++ fibonachi/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fibonachi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // INPUT
+            Console.Write("Колко числа да генерира ? - ");
+            byte n = byte.Parse(Console.ReadLine()); 
+
+            List<long> num = new List<long>();
+            num.Add(1);
+            num.Add(1);
+
+            for (int i = 2; i < n; i++)
+            {
+                num.Add(num[i - 2] + num[i - 1]);
+            }
+
+            // OUTPUT
+
+            Console.WriteLine(string.Join(", ", num));
+        }
+    }
+}
